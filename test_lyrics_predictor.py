9c6e6d45a08192332703733796c7cbf2c7e3a690
Amirali Yazdi <amirali.yazdi@yahoo.com>
2020-03-17 16:53:30 +0100

    add file

--- test_lyrics_predictor.py
+++ test_lyrics_predictor.py
@@ -1,7 +1,19 @@
 import lyrics_predictor
+import pytest
 
-# test getting urls of first two pages
-@pytest.mark.parametrize(['artist', 'pages'], ["Red Hot Chillie Pepers",
-                          "Metallica", "Papa Roach", "Shakira", "Beyonce"], 2 )
-def test_urls(artist, pages):
-    assert len(lyrics_predictor.get_urls(artist)) >= pages
+# test urls genrating of the first two pages
+PASSING_CONDITIONS=["Red Hot Chillie Pepers", "Metallica", "Papa Roach", "Shakira", "Beyonce"]
+
+@pytest.mark.parametrize("artist", PASSING_CONDITIONS)
+def test_urls(artist):
+    urls, name = lyrics_predictor.get_urls(artist)
+    assert len(urls) == 2
+    
+
+
+# FAILING_CONDITIONS=["XGSHDN", "Tiger Woods", "121212","Donald Trump", "king kong"]
+# # test getting urls of first two pages with fake artists name
+# @pytest.mark.parametrize('artist', FAILING_CONDITIONS)
+# def test_urls(artist):
+#     name, urls = lyrics_predictor.get_urls(artist)
+#     assert len(urls) > 1
