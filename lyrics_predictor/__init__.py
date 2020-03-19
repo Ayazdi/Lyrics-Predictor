4e9789c3473d44b1aaed7b38636678d9e337dd93
Amirali Yazdi <amirali.yazdi@yahoo.com>
2020-03-19 16:22:33 +0100

    delete space

--- lyrics_predictor/__init__.py
+++ lyrics_predictor/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 """Top-level package for lyrics-predictor."""
-#from . import vectorize_and_train
+
 __author__ = """Amirali Yazdi"""
 __email__ = 'amirali.yazdi@yahoo.com'
 __version__ = '0.1.0'
